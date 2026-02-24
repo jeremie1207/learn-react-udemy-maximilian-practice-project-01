8ff83ad0abbdd145dcda1b34c8b9872f9475b516
Jeremie Barhabazi <71102646+jeremie1207@users.noreply.github.com>
2026-02-24 09:45:55 -0500

    Add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import investmentLogo from '../assets/investment-calculator-logo.png';
+
+export default function Header(){
+    return <header id="header">
+        <img src={investmentLogo} alt="investment calculator logo" />
+        <h1 >Investment Calculator</h1>
+    </header>
+}
