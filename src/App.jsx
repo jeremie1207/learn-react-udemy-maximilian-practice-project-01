cb01688751899ceda78c1c5bf494fd77834d0375
Jeremie Barhabazi <71102646+jeremie1207@users.noreply.github.com>
2026-02-24 14:43:47 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
 import Result from "./components/Result";
+import { useState } from "react";
+import { calculateInvestmentResults, formatter } from "./util/investment";
 
 function App() {
+
+  const [formData, setFormData] = useState({
+        initialInvestment: 0,
+        annualInvestment: 0,
+        expectedReturn: 0, 
+        duration: 0
+    });
+
+    const [result, setResult] = useState([]);
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setFormData( prev => {
+            const updated = {
+                ...prev,
+                [name]: +value
+            };
+            
+            console.log(calculateInvestmentResults(updated));
+            setResult(calculateInvestmentResults(updated));
+            
+            return updated;
+        });
+    }
+
   return (
     <>
       <Header />
-      <UserInput />
-      <Result />
+      <UserInput formData={formData} handleChange={handleChange}/>
+      <Result result={result}/>
     </>
   )
 }
 
 export default App
-
