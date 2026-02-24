f8703b79d6ead5109ab3356c5b92e98d961b606a
Jeremie Barhabazi <71102646+jeremie1207@users.noreply.github.com>
2026-02-24 12:43:23 -0500

    add user input form

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+export default function UserInput(){
+
+    const [formData, setFormData] = useState({
+        initialInvestment:"",
+        annualInvestment:"",
+        expectedReturn:"", 
+        duration:""
+    });
+
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value
+        }));
+    }
+
+
+    return <form action="" id="user-input">
+        <div className="input-group">
+            <div>
+                <label htmlFor="initial_investment">Initial Investment</label>
+                <input 
+                    type="number" 
+                    name="initialInvestment" 
+                    id="initial_investment"
+                    value={formData.initialInvestment}
+                    onChange={handleChange}
+                    />
+            </div>
+            <div>
+                <label htmlFor="annual_investment">Annual Investment</label>
+                <input 
+                    type="number" 
+                    name="annualInvestment" 
+                    id="annual_investment" 
+                    value={formData.annualInvestment} 
+                    onChange={handleChange}/>
+            </div>
+        </div>
+
+        <div className="input-group">
+            <div>
+                <label htmlFor="expected_return">Expected Return</label>
+                <input 
+                    type="number" 
+                    name="expectedReturn" 
+                    id="expected_return" 
+                    value={formData.expectedReturn} 
+                    onChange={handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="duration">Duration</label>
+                <input 
+                    type="number" 
+                    name="duration" 
+                    id="duration" 
+                    value={formData.duration} 
+                    onChange={handleChange}/>
+            </div>
+        </div>
+    </form>
+}
