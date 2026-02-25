7861a79892ac350d66806fa7766c632e9a8e2262
Jeremie Barhabazi <71102646+jeremie1207@users.noreply.github.com>
2026-02-25 06:55:26 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import UserInput from "./components/UserInput";
 import Result from "./components/Result";
 import { useState } from "react";
-import { calculateInvestmentResults, transformAnnualData, formatter } from "./util/investment";
+import { calculateInvestmentResults, transformAnnualData } from "./util/investment";
 
 function App() {
 
